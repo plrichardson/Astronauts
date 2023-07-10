213b7fc714252c12c7802393bf71d492ab74fdc6
Paul Richardson <developer@photographerafoot.com>
2023-07-10 10:51:28 +0300

    Add astronautsView

---
+++ Astronauts/Astronauts/Views/AstronautsView.swift
@@ -0,0 +1,20 @@
+//
+//  AstronautsView.swift
+//  Astronauts
+//
+//  Created by Paul Richardson on 10/7/2023.
+//
+
+import SwiftUI
+
+struct AstronautsView: View {
+    var body: some View {
+        Text("Astronauts")
+    }
+}
+
+struct AstronautsView_Previews: PreviewProvider {
+    static var previews: some View {
+        AstronautsView()
+    }
+}
