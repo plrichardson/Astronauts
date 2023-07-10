5ee26390929ee6da0f6f293b1cb33785efb4358b
Paul Richardson <developer@photographerafoot.com>
2023-07-10 10:41:57 +0300

    Initial Commit

---
+++ Astronauts/AstronautsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AstronautsApp.swift
+//  Astronauts
+//
+//  Created by Paul Richardson on 10/7/2023.
+//
+
+import SwiftUI
+
+@main
+struct AstronautsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AstronautsUITests/AstronautsUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  AstronautsUITestsLaunchTests.swift
+//  AstronautsUITests
+//
+//  Created by Paul Richardson on 10/7/2023.
+//
+
+import XCTest
+
+final class AstronautsUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
